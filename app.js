048a92ccd3054738538e9363e6cbf13deaf59224
Chris Ekstedt <9835700+cekstedt@users.noreply.github.com>
2022-06-27 08:36:06 -0700

    add POST routes for /register and /login. Add mongoose to store user info to DB.

--- app.js
+++ app.js
@@ -4,17 +4,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 // Setting up imports for use.
 
 const app = express();
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+mongoose.connect(process.env.MONGO_DB);
 
 // Global Variables.
 
 const PORT = process.env.PORT;
+
+const userSchema = new mongoose.Schema({ user: String, password: String });
+const User = mongoose.model("User", userSchema);
 
 // GET routes.
 
@@ -28,6 +33,38 @@ app.get("/login", function(req, res) {
 
 app.get("/register", function(req, res) {
   res.render("register");
+});
+
+// POST routes.
+
+app.post("/register", function(req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+  newUser.save(function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/login", function(req, res) {
+  User.findOne({ email: req.body.username }, function(err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else if (foundUser) {
+      if (foundUser.password === req.body.password) {
+        res.render("secrets");
+      } else {
+        // Handle wrong password.
+      }
+    } else {
+      // Handle user not found.
+    }
+  });
 });
 
 // Initialize server.

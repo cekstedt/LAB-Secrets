56c5580e4df581cbc550cce6819dcb6c7e796617
Chris Ekstedt <9835700+cekstedt@users.noreply.github.com>
2022-06-30 10:28:51 -0700

    Update app to use passport-local-mongoose and express-session to handle local authentication.

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 // Setting up imports for use.
 
@@ -14,15 +15,26 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+  })
+);
 mongoose.connect(process.env.MONGO_DB);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Global Variables.
 
 const PORT = process.env.PORT;
-
-const userSchema = new mongoose.Schema({ email: String, password: String });
-
+const userSchema = new mongoose.Schema({ username: String, password: String });
+userSchema.plugin(passportLocalMongoose);
 const User = mongoose.model("User", userSchema);
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // GET routes.
 
@@ -38,45 +50,58 @@ app.get("/register", function(req, res) {
   res.render("register");
 });
 
+app.get("/secrets", function(req, res) {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/logout", function(req, res) {
+  req.logout(function(err) {
+    if (err) {
+      console.log(err);
+      res.redirect("/secrets");
+    } else {
+      res.redirect("/");
+    }
+  });
+});
+
 // POST routes.
 
 app.post("/register", function(req, res) {
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    const newUser = new User({
-      email: req.body.username,
-      password: hash
-    });
-    newUser.save(function(err) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("secrets");
-      }
-    });
+  User.register({ username: req.body.username }, req.body.password, function(
+    err,
+    user
+  ) {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        // Only fires if authentication was successful.
+        res.redirect("/secrets");
+      });
+    }
   });
 });
 
 app.post("/login", function(req, res) {
-  User.findOne({ email: req.body.username }, function(err, foundUser) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  req.login(user, function(err) {
     if (err) {
       console.log(err);
-    } else if (foundUser) {
-      bcrypt.compare(req.body.password, foundUser.password, function(
-        err,
-        result
-      ) {
-        if (err) {
-          console.log(err);
-        } else if (result) {
-          res.render("secrets");
-        } else {
-          // Handle wrong password.
-          console.log("Wrong password");
-        }
-      });
+      res.redirect("/login");
     } else {
-      // Handle user not found.
-      console.log("User not found");
+      passport.authenticate("local")(req, res, function() {
+        // Only fires if authentication was successful.
+        res.redirect("/secrets");
+      });
     }
   });
 });
